cc1bbefd444d02250146d808e011fca578d2cf16
Jelani Thomas <jelani13@icloud.com>
2021-03-09 15:08:37 -0500

    initial commit

---
+++ connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection( 
+    {
+        host: "localhost",
+        port: 3000,
+        user: rootCertificates,
+        password: "password",
+        database: "employeeDB"
+    }
+);
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection

33ded8303279f99a42bc4678984a47cc6d046b38
Jelani Thomas <jelani13@icloud.com>
2021-03-12 09:01:12 -0500

    moved seeds file into db folder started on linking foreign keys

--- db/employeeDB.sql
+++ db/employeeDB.sql
@@ -7,15 +7,16 @@ CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT, 
     name VARCHAR(30)  UNIQUE NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
     INDEX dep_id (department_id),
-    title VARCHAR(30),
+    title VARCHAR(30) NOT NULL,
     salary INT UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL
-    --FK
-)
+    department_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+
+);
 
 CREATE TABLE employee (
     id INT UNIQUE AUTO_INCREMENT ,
@@ -23,9 +24,9 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     INDEX role_id (role_id),
-    --FOREIGN KEY REFERENCES, 
+    FOREIGN KEY (role_id) REFERENCES employee(manager_id) ON DELETE CASCADE,
     manager_id INT UNSIGNED,
     INDEX man_id (manager_id),
-    --FOREIGN KEY REFERENCES,
+    FOREIGN KEY (manager_id) REFERENCES employee(role_id) ON DELETE CASCADE,
     PRIMARY KEY(id)
-)
+);
